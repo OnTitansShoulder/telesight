f05d4710aafd094aa17de6927b78059d1e263583
Zhongkai Liu <liu676785882@gmail.com>
2021-10-17 15:22:05 -0700

    fix stop script

--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if systemctl list-units --full -all | grep telesight | awk '{printf "%s %s %s %s\n", $1,$2,$3,$4}'; then
+if service telesight status > /dev/null; then
     sudo systemctl stop telesight
     sudo systemctl disable telesight
 fi
-if systemctl list-units --full -all | grep mjpg_streamer | awk '{printf "%s %s %s %s\n", $1,$2,$3,$4}'; then
+if service mjpg_streamer status > /dev/null; then
     sudo systemctl stop mjpg_streamer
     sudo systemctl disable mjpg_streamer
 fi
